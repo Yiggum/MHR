14c4d9fff31f7d87ed99bf3ae984596fc2435483
Yiggum <yiggum123@gmail.com>
2022-01-15 15:22:46 +0000

    Ap1T9

--- script.js
+++ script.js
@@ -1,45 +1,104 @@
+const firstName = document.getElementById("firstName");
+const emailAdress = document.getElementById("email");
+const cardNumEntered = document.getElementById("cardDetails")
+
+function validatorCaller() {
+ var a = a
+if(validateFormName(firstName) && 
+validateFormEmail(emailAdress) &&
+validateFormCard()) {
+    return true;
+} else {
+  return false;
+}
+}
+
+
 function validateFormName()
   {
-  var firstName = document.getElementById("firstName")
+  // var firstName = document.getElementById("firstName");
    var letters = /^[A-Za-z]+$/;
-   
-///^[A-Za-z][0-9]+$@./
 
    if(!firstName.value.match(letters))
      {
-       alert("Please enter your name");
-
+       alert("Please enter a valid name");
       return false;
+      
      }
      
    else {
-     true;
+     
+     return true;
    }
   }
 
 
-// function validateFormEmail()
-// {
-//   var emailAdress = document.getElementById("email")
-//    var email = /^[A-Za-z]+$/;
-
-//    if(!emailAdress.value.match(email))
-//      {
-//        alert("Please enter your name");
-
-//       return false;
-//      }
-     
-//    else {
-//      true;
-//    }
-//   }
-
-   ///^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-
-
-
-
-
-       
+function validateFormEmail()
+{
+ 
+  var regex =   /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
   
+   if(!emailAdress.value.match(regex))
+     {
+       alert("Please enter a valid email address");
+      return false;
+     }     
+   else {
+     return true;
+   }
+}
+
+
+function luhnValidator(value) {
+  // accept only digits, dashes or spaces
+    
+  if (!value){
+   return false;
+  }
+      if (/[^0-9-\s]+/.test(value)) return false;
+  
+  // The Luhn Algorithm. It's so pretty.
+      var nCheck = 0, nDigit = 0, bEven = false;
+      value = value.replace(/\D/g, "");
+  
+      for (var n = value.length - 1; n >= 0; n--) {
+          var cDigit = value.charAt(n),
+              nDigit = parseInt(cDigit, 10);
+  
+          if (bEven) {
+              if ((nDigit *= 2) > 9) nDigit -= 9;
+          }
+  
+          nCheck += nDigit;
+          bEven = !bEven;
+      }
+        return (nCheck % 10) == 0;
+    }
+   
+    
+function validateFormCard() {
+  
+  if (luhnValidator(cardNumEntered.value)) {
+    return true;
+  } else {
+    alert("Please enter a valid Credit card number")
+    return false;
+  }
+}
+
+
+// console.log("the console log for card validator itself = " + luhnValidator("4417 1234 5678 9113"));
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+//4417 1234 5678 9113
